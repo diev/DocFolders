e5534a90813a06733134fe48857ac3a4c444cc16
Dmitrii Evdokimov <diev@mail.ru>
2016-04-16 15:16:02 +0300

    Add classes to Model.

---
+++ DocFolders/src/DocFolders/Models/ItemLink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocFolders.Models
+{
+    public class ItemLink
+    {
+        public string OuterId { get; set; }
+        public string InnerId { get; set; }
+    }
+}
---
+++ DocFolders/src/DocFolders/Models/LinkItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocFolders.Models
+{
+    public class LinkItem
+    {
+        public string Id { get; set; }
+        public string Caption { get; set; }
+        public string Source { get; set; }
+    }
+}
